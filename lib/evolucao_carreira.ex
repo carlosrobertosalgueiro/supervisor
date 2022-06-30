e1e730c51f4e3fdb7fea8450d31abde7d2b694f0
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-06-30 08:40:53 -0300

    insert fuction buscar pessa

--- lib/evolucao_carreira.ex
+++ lib/evolucao_carreira.ex
@@ -1,3 +1,19 @@
 defmodule EvolucaoCarreira do
+  use GenServer
+  # server
+  def init({idade, salario}) do
+    {:ok, {idade, salario}}
+  end
 
+  def start_link({nome, idade, salario}) do
+    GenServer.start_link(__MODULE__, {idade, salario}, name: nome)
+  end
+
+  def handle_call(:get, _from, state) do
+    {:reply, state, state}
+  end
+
+  # client
+
+  def buscar_pessoa(nome), do: GenServer.call(nome, :get)
 end
